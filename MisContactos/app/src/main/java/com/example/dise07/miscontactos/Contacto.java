98e089832816bc95de6b89de69b7faf4e89955b0
poolelera <exxel.elera@unmsm.edu.pe>
2016-12-26 18:57:27 -0500

    MisContactos

---
+++ MisContactos/app/src/main/java/com/example/dise07/miscontactos/Contacto.java
@@ -0,0 +1,55 @@
+package com.example.dise07.miscontactos;
+
+/**
+ * Created by Dise07 on 08/11/2016.
+ */
+
+public class Contacto {
+
+    private String nombre;
+    private String tlefono;
+    private String email;
+    private int foto;
+
+
+    public Contacto(String nombre, String tlefono, String email, int foto) {
+        this.nombre     = nombre;
+        this.tlefono    = tlefono;
+        this.email      = email;
+        this.foto       = foto;
+
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+
+    public String getTlefono() {
+        return tlefono;
+    }
+
+    public void setTlefono(String tlefono) {
+        this.tlefono = tlefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
---
+++ MisContactos/app/src/main/java/com/example/dise07/miscontactos/DetalleContacto.java
@@ -0,0 +1,78 @@
+package com.example.dise07.miscontactos;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.Image;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DetalleContacto extends AppCompatActivity {
+
+    private TextView tvNombre;
+    private TextView tvTelefono;
+    private TextView tvEmail;
+    private int tvImagen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_contacto);
+
+        Bundle parametros = getIntent().getExtras();
+
+        String nombre = parametros.getString("nombre");
+        String telefono = parametros.getString("telefono");
+        String email = parametros.getString("email");
+
+        tvNombre = (TextView) findViewById(R.id.tvNombre);
+        tvTelefono = (TextView) findViewById(R.id.tvTel);
+        tvEmail = (TextView) findViewById(R.id.tvEmail);
+
+        tvNombre.setText(nombre);
+        tvTelefono.setText(telefono);
+        tvEmail.setText(email);
+
+    }
+
+    public void llamar(View v) {
+        String telefono = tvTelefono.getText().toString();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + telefono))); // El teléfono es un recurso tipo URI
+    }
+
+    public void enviarMail(View v){
+        String email = tvEmail.getText().toString();
+        Intent emailIntent = new Intent((Intent.ACTION_SEND));
+        emailIntent.setData(Uri.parse("mailto"));
+
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, email); //EXTRA_CC, EXTRA_SUBJECT, EXTRA_HTML_TEXT,etc
+        emailIntent.setType("message/rcf822");
+        startActivity(Intent.createChooser(emailIntent, "Email "));
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+
+        if(keyCode == KeyEvent.KEYCODE_BACK) {
+            Intent intent = new Intent (DetalleContacto.this, MainActivity.class);
+            startActivity(intent);
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
